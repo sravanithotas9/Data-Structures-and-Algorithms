70f3ae4ebdbfc27d3131fa19392af130e74a4d68
sravani <sravanithotas9@gmail.com>
2023-12-21 11:36:35 -0800

    corrected code

--- coding/java/AllDissappearedNum.java
+++ coding/java/AllDissappearedNum.java
@@ -7,48 +7,35 @@ import java.util.List;
 
 public class AllDissappearedNum {
     public static void main(String[] args) {
-        int[] arr = {4, 3, 2, 6,3,8, 2, 3};
-        System.out.println (findDisappearedNumbers ( arr ));
-
+        int[] arr = {4,3,2,7,8,2,3,1};
+        System.out.println ( findDisappearedNumbers ( arr ) );
     }
 
-    /* public static int allDissappeared(int[] arr) {
-          Arrays.sort ( arr );
-          int i = 1, num = 0;
-          while (i < arr.length) {
-
-              if (arr[i] - arr[i - 1] == 1) {
-
-              } else {
-                  num = arr[i - 1] + 1;
-
-              }
-              i++;
-
-          }
-
-         return num;
-     }
-  }*/
     public static List<Integer> findDisappearedNumbers(int[] arr) {
         ArrayList<Integer> result = new ArrayList<> ();
 
-        for (int n : arr) {
-            int i = Math.abs(n)-1;
-            if (arr[i] > 0) {
-                arr[i] =-arr[i];
+        for (int nums : arr) {
+            int index = Math.abs ( nums ) - 1;
+            if (arr[index] > 0) {
+                arr[index] = -arr[index];
             }
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) {
-                result.add(i+1);
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > 0) {
+                    result.add ( i + 1 );
+                }
             }
-        }
 
-        return result;
+            return result;
+
     }
 }
+
+
+
+
+
 
 
 
